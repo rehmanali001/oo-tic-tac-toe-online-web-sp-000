10c9ae95d7d6ef21c4a236d5ea5fa4a02f71dcbc
Rehman Ali <rehmanali001@gmail.com>
2019-08-01 15:54:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,9 +15,9 @@ class TicTacToe
     ]
   def display_board
     puts "#{board[0]} | #{board[1]} | #{board[3]}"
-    puts "---------------------------------------"
+    puts "-----------"
     puts "#{board[4]} | #{board[5]} | #{board[6]}"
-    puts "---------------------------------------"
+    puts "-----------"
     puts "#{board[7]} | #{board[8]} | #{board[9]}"
   end
 end
