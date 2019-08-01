cea1433e20b4b588846b4bbcfdfbf99ceb5c493f
Rehman Ali <rehmanali001@gmail.com>
2019-08-01 16:05:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,5 +31,9 @@ class TicTacToe
   end
   
   def position_take?
-    
+      if @board[index] == " " || @board[index] == "" || @board[index] == nil
+    false
+  elsif @board[index] == "X" || @board[index] == "O"
+    true
+  end
 end
