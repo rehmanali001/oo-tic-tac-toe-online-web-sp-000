6bba7fe7190ee9d0c13cf609c9df25fee5f6850e
Rehman Ali <rehmanali001@gmail.com>
2019-08-01 16:08:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,11 +39,6 @@ class TicTacToe
   end
   
   def valid_move?(index)
-      if index.between?(0, 8) && !position_taken?(board, index)
-    true
-  else
-    false
+    !(@board[index].nil? || @board[index] == " ")
   end
-  end
-  
 end
