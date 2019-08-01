49e01017ea912cd48440a187411b4593a9c4c85e
Rehman Ali <rehmanali001@gmail.com>
2019-08-01 16:11:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,6 +47,12 @@ class TicTacToe
   end
   
   def turn_count
-    
+      counter = 0
+  board.each do |moves| 
+  if moves == "X" || moves == "O"
+    counter += 1
+  end
+  end
+  counter
     
 end
